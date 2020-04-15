b388281fb43893bde0fb516d9000ebc5216218ee
Sagee Conway <sageeconway@gmail.com>
2020-04-15 18:38:59 +0300

    adding public path for gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/developer-toold/" : "/",
   css: {
     loaderOptions: {
       sass: {
-        prependData: `@import "@/scss/_variables.scss";`
-      }
-    }
-  }
+        prependData: `@import "@/scss/_variables.scss";`,
+      },
+    },
+  },
 };
